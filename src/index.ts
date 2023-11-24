9bf4060c9d42b45c91cd796ea9548c9f3d08d542
Rauli Laine <rauli.laine@iki.fi>
2023-11-24 19:23:42 +0200

    Export toSource and toString functions

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export * from "./context";
 export * from "./parser";
+export { valueToSource as toSource } from "./to-source";
+export { valueToString as toString } from "./to-string";
 export * from "./types";
